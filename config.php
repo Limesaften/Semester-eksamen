1da17100f488f43c7aeac5abbc846f4360bf5605
Limesaften <Madshoimark@gmail.com>
2017-12-19 12:15:59 +0100

    semester eksamen

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+$DB_HOST = 'localhost';
+$DB_USER = '#';
+$DB_PASS = '#';
+$DB_NAME = 'hobbitten';
+
+$link = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+if ($link->connect_error) {
+    die('Connect Error ('.$link->connect_errno.') '.$link->connect_error);
+}
+$link->set_charset('utf8');
+?>
